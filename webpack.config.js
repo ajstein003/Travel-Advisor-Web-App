997991e38892a5f74157078b967b5d43d49dfb50
Arjab Chakrabarti <124612257+ajstein003@users.noreply.github.com>
2023-07-09 00:58:29 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // Add any other necessary loaders for CSS, images, etc.
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
